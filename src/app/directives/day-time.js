fff1aefd6bde192eaf2c912bd9f1343174ff1f5d
Acácio Osório <acacio@samuraicoder.com.br>
2017-01-30 00:09:09 -0200

    day class fix

--- src/app/directives/day-time.js
+++ src/app/directives/day-time.js
@@ -7,16 +7,15 @@
 		return {
 			restrict: 'A',
 			link : function(scope, el, attr) {
-
-				var dayTime = new Date(); 
-		  
-				if ( dayTime.getHours() < 12 )  {
+				var dayTime = new Date();
+				console.log(dayTime.getHours());
+				if ( dayTime.getHours() >= 5 && dayTime.getHours() <= 17 )  {
 					el.addClass('day-weather');
 				} 
-				else if ( dayTime.getHours() >= 12 && dayTime.getHours() <= 17 ) { 
+				else if ( dayTime.getHours() >= 17 && dayTime.getHours() <= 20 ) { 
 					el.addClass('sunset-weater');
 				} 
-				else if ( dayTime.getHours() > 17 && dayTime.getHours() <= 24 ) { 
+				else if ( dayTime.getHours() >= 20 && dayTime.getHours() <= 5 ) { 
 					el.addClass("night-weather");
 				} 
 				else {
